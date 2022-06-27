fb62531a3e2e7045364ed44315e41dd62cab0217
Saad <saad21290@gmail.com>
2022-06-27 11:04:32 -0700

    Update advection2D_generate_train.jl

--- examples/advection2D_generate_train.jl
+++ examples/advection2D_generate_train.jl
@@ -17,18 +17,22 @@ default(fontfamily="serif",frame=:box,grid=:hide,palette=:viridis,markeralpha=0.
 #PyPlot.matplotlib.rc("mathtext",fontset="cm")
 
 @with_kw mutable struct Args
-    num_sensors::Int = 144;
+    num_sensors_x::Int = 64;
+    num_sensors_y::Int = 1;
+
     num_train_functions::Int = 500;
     num_test_functions::Int = Int(2*num_train_functions);
     num_sol_points::Int = 100;
     L_x = [0.0 1.0]';
-    L_y = [0.0 1.0];
-    M_x = 6;
-    M_y = 6;
-    alp = [2.0 1.0];
+    L_y = [0.0 0.0];
+    M_x = 8;
+    M_y = 0;
+    alp = [1.0 0.0];
 
     tspan::Tuple = (0.0,1.0);
-    n_epoch::Int = 25000;
+    n_epoch::Int = 7500;
+
+    num_sensors = Int(num_sensors_x*num_sensors_y);
 
     N::Int = num_sensors;
     branch_layers::Int = 2; # Branch depth
@@ -44,7 +48,7 @@ function generate_train(pde_function;kws...)
     args = Args(;);
 
     # Generate training and testing data
-    train_data, test_data = generate_periodic_train_test_Adv2D(args.L_x,args.L_y,args.M_x,args.M_y,args.tspan,args.alp,args.num_sensors,args.num_train_functions,args.num_test_functions,args.num_sol_points);
+    train_data, test_data = generate_periodic_train_test_Adv2D(args.L_x,args.L_y,args.M_x,args.M_y,args.tspan,args.alp,args.num_sensors_x,args.num_sensors_y,args.num_train_functions,args.num_test_functions,args.num_sol_points);
     save_data(train_data,test_data,args.num_train_functions,args.num_test_functions,args.num_sol_points,pde_function)
 
     branch = build_dense_model(args.branch_layers,args.branch_neurons,args.branch_activations);
